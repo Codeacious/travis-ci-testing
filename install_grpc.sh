116ad99cd109e0113d5d9387a3e46afb6b9860ed
Rob McGuinness <forexviii@gmail.com>
2017-08-02 16:16:15 -0700

    make install_grpc simpler

--- install_grpc.sh
+++ install_grpc.sh
@@ -2,18 +2,18 @@
 set -ex
 
 # Install protobuf
-pushd $TRAVIS_BUILD_DIR/..
-git clone https://github.com/google/protobuf.git
+# pushd $TRAVIS_BUILD_DIR/..
+# git clone https://github.com/google/protobuf.git
 
-pushd protobuf
-./autogen.sh
-./configure --prefix=/usr
-make && make check && sudo make install && sudo ldconfig
-popd
+# pushd protobuf
+# ./autogen.sh
+# ./configure --prefix=/usr
+# make && make check && sudo make install && sudo ldconfig
+# popd
 
-popd
+# popd
 
-# Install grpc
+# Install grpc (this should install protobuf)
 pushd $TRAVIS_BUILD_DIR/..
 git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
 
